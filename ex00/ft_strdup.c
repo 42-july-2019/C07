2ccfc6e9c55bf387d41129bf680d24023438e765
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-16 12:03:57 +0200

    Add ex00

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/16 10:48:53 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/16 12:02:26 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		length;
+	char	*result;
+
+	length = 0;
+	while (src[length])
+		length++;
+	result = (char *)malloc(sizeof(char) * length);
+	i = 0;
+	while (src[i])
+	{
+		result[i] = src[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
