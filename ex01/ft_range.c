70a4fbcc798c82c23e2b09b8c0629f248db99301
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-16 12:32:11 +0200

    Fix norm fo ex01

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -6,13 +6,13 @@
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 20:08:19 by exam              #+#    #+#             */
-/*   Updated: 2019/07/16 12:23:16 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/16 12:31:54 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int     *ft_range(int min, int max)
+int		*ft_range(int min, int max)
 {
 	int i;
 	int length;
@@ -21,8 +21,8 @@ int     *ft_range(int min, int max)
 	tab = 0;
 	i = 1;
 	if (min >= max)
-		return tab;
-	length =  max - min;
+		return (tab);
+	length = max - min;
 	tab = malloc(sizeof(int) * (length) - 1);
 	tab[0] = min;
 	while (i < length)
@@ -30,5 +30,5 @@ int     *ft_range(int min, int max)
 		tab[i] = tab[i - 1] + 1;
 		i++;
 	}
-	return tab;
+	return (tab);
 }
